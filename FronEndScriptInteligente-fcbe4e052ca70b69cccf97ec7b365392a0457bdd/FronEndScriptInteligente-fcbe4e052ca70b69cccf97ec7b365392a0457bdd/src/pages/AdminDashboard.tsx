78654be85872f91e49fb0631108731061eb26d87
Discogiordano <88466550+Discogiordano@users.noreply.github.com>
2024-06-19 04:21:38 -0300

    front 2

--- FronEndScriptInteligente-fcbe4e052ca70b69cccf97ec7b365392a0457bdd/FronEndScriptInteligente-fcbe4e052ca70b69cccf97ec7b365392a0457bdd/src/pages/AdminDashboard.tsx
+++ FronEndScriptInteligente-fcbe4e052ca70b69cccf97ec7b365392a0457bdd/FronEndScriptInteligente-fcbe4e052ca70b69cccf97ec7b365392a0457bdd/src/pages/AdminDashboard.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import "../styles/adminDashboard.css";
-import gerenciarUsuariosIcon from "./images/gerenciar_usuarios.ico"; // Importe a imagem
+import gerenciarUsuariosIcon from "./images/gerenciar_usuarios.ico"; 
+import visualizarLogsIcon from "./images/visualizar_logs.ico"; 
+import supervisionarSubmissoesIcon from "./images/supervisionar_submissoes.ico"; 
+import settingsIcon from "./images/settings.ico"; 
 
 const AdminDashboard: React.FC = () => {
   const navigate = useNavigate();
@@ -20,6 +23,7 @@ const AdminDashboard: React.FC = () => {
           Visualizar Script do Usuário
         </button>
         <button onClick={() => navigate("/configure-integrations")}>
+          <img src={settingsIcon} alt="Configurar Integrações" className="icon" />
           Configurar Integrações do Sistema
         </button>
         <button onClick={() => navigate("/manage-users")}>
@@ -27,9 +31,11 @@ const AdminDashboard: React.FC = () => {
           Gerenciar todos os perfis
         </button>
         <button onClick={() => navigate("/manage-scripts")}>
+          <img src={supervisionarSubmissoesIcon} alt="Supervisionar Submissões" className="icon" />
           Supervisionar todas as submissões
         </button>
         <button onClick={() => navigate("/audit-activities")}>
+          <img src={visualizarLogsIcon} alt="Auditar Atividades" className="icon" />
           Auditar Atividades do Sistema
         </button>
         <button onClick={() => navigate("/system-maintenance")}>
