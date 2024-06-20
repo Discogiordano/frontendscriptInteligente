f9f3c401899e166a4966d4dd360d3d16dac05e0c
Discogiordano <88466550+Discogiordano@users.noreply.github.com>
2024-06-20 00:27:39 -0300

    front features 1

---
+++ FronEndScriptInteligenteee/FronEndScriptInteligenteee/images.d.ts
@@ -0,0 +1,48 @@
+declare module "*.jpg" {
+    const value: string;
+    export default value;
+  }
+  
+  declare module "*.png" {
+    const value: string;
+    export default value;
+  }
+  
+  declare module "*.ico" {
+    const value: string;
+    export default value;
+  }
+  // declare outros formatos de imagens:
+  // declare module "*.gif" {
+  //   const value: string;
+  //   export default value;
+  // }
+  // declare module "*.svg" {
+  //   const value: string;
+  //   export default value;
+  // }
+  // declare module "*.webp" {
+  //   const value: string;
+  //   export default value;
+  // }
+  // declare module "*.bmp" {
+  //   const value: string;
+  //   export default value;
+  // }
+  // declare module "*.tiff" {
+  //   const value: string;
+  //   export default value;
+  // }
+  // declare module "*.ico" {
+  //   const value: string;
+  //   export default value;
+  // }
+  // declare module "*.cur" {
+  //   const value: string;
+  //   export default value;
+  // }
+  // declare module "*.webp" {
+  //   const value: string;
+
+  
+  
